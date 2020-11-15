289cdab3a974055d304fa64551c73400d60ee797
Miftahul Huda <miftahulxhuda@gmail.com>
2020-11-15 13:04:40 +0700

    Section 6 - 15. Reusable State Updates

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -13,12 +13,21 @@ const SquareScreen = () => {
         // color === 'red', 'green', 'blue'
         // change === +15, -15
 
-        if(color === 'red') {
-            if(red + change > 255 || red + change < 0) {
-                return;
-            } else {
-                setRed(red + change);
-            }
+        switch (color) {
+            case 'red':
+                red + change > 255 || red + change < 0 ? null : setRed(red + change);
+                break;
+
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+                break;
+
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+                break;
+        
+            default:
+                break;
         }
     };
 
@@ -30,13 +39,13 @@ const SquareScreen = () => {
                 color="Red" 
             />
             <ColorCounter 
-                onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-                onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+                onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+                onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
                 color="Blue" 
             />
             <ColorCounter 
-                onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-                onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+                onIncrease={() => setColor('green', COLOR_INCREMENT)}
+                onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
                 color="Green" 
             />
             <View 
