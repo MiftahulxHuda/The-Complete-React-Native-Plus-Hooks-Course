8f9c125af95827c776bf869619e10b2548440b30
Miftahul Huda <huda@tura.group>
2020-11-20 16:54:54 +0700

    Section 11 - 3. A Few More Styling Issues

--- food/src/components/ResultsDetail.js
+++ food/src/components/ResultsDetail.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View, Image, Text, StyleSheet} from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
-const ResultsDetail = ({result}) => {
+const ResultsDetail = ({ result }) => {
     return (
         <View>
-            <Image styles={styles.image} source={{uri: result.image_url}} />
+            <Image style={styles.image} source={{ uri: result.image_url }} />
             <Text style={styles.name}>{result.name}</Text>
             <Text>
                 {result.rating} Stars, {result.review_count} Reviews
@@ -14,10 +14,14 @@ const ResultsDetail = ({result}) => {
 };
 
 const styles = StyleSheet.create({
+    container: {
+        marginLeft: 15
+    },
     image: {
         width: 250,
         height: 120,
-        borderRadius: 4
+        borderRadius: 4,
+        marginBottom: 5
     },
     name: {
         fontWeight: 'bold'
--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -21,7 +21,12 @@ const ResultsList = ({title, results}) => {
 const styles = StyleSheet.create({
     title: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10
     }
 });
 
