95b4c212b6973cb4b4d79dc9718676cca0671d68
Miftahul Huda <huda@tura.group>
2020-11-22 17:06:20 +0700

    Section 11 - 7. Spacing on the Search Bar

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -19,7 +19,6 @@ const SearchScreen = () => {
         <>
             <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results</Text>
             <ScrollView>
                 <ResultsList 
                     results={filterResultsByPrice('$')} 
