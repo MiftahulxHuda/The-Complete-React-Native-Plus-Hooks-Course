c7f00e3dc77a0cb0256ece16e6358dc77fde6648
Miftahul Huda <huda@tura.group>
2020-11-26 13:46:26 +0700

    Section 12 - 25. Saving a New Post

--- blog/src/screens/CreateScreen.js
+++ blog/src/screens/CreateScreen.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const ShowScreen = ({navigation}) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const {addBlogPost} = useContext(Context);
     
     return (
         <View>
@@ -20,7 +21,10 @@ const ShowScreen = ({navigation}) => {
                 value={content} 
                 onChangeText={(text) => setContent(text)} 
             />
-            <Button title="Add Blog Post" />
+            <Button 
+                title="Add Blog Post" 
+                onPress={() => addBlogPost(title, content)}
+            />
         </View>
     );
 };
