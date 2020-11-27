d98ea6d7882b0bb33caac12fce5009f2a35b291e
Miftahul Huda <huda@tura.group>
2020-11-27 17:42:16 +0700

    Section 13 - 9. Deleting a Post

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -44,7 +44,9 @@ const addBlogPost = dispatch => {
     };
 };
 const deleteBlogPost = dispatch => {
-    return (id) => {
+    return async id => {
+        await jsonServer.delete(`/blogposts/${id}`);
+
         dispatch({ type: 'delete_blogpost', payload: id })
     };
 };
