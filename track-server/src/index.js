4bdf8eeb166f61c8880fc18308125b5cb061e8d1
Miftahul Huda <huda@tura.group>
2020-11-28 04:09:37 +0700

    Section 14 - 5. Connecting to MongoDB

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb://localhost:27017/track_server';
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
